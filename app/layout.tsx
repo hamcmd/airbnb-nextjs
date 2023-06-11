082fcc889586061ce80aad1fc8bc3c7f57d0b400
hamcmd <mohammad.hammad@dealcart.io>
2023-06-12 01:34:53 +0500

    initial check in

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+
+import { Nunito } from "next/font/google";
+
+import Navbar from "./components/navbar/Navbar";
+
+const font = Nunito( {
+  subsets: [ "latin" ],
+} );
+
+export const metadata = {
+  title: "Airbnb: Vacation Rentals, Cabins, Beach Houses, Unique Homes & Experiences",
+  description: "Find vacation rentals, cabins, beach houses, unique homes and experiences around the world - all made possible by hosts on Airbnb.",
+};
+
+export default function RootLayout( {
+  children,
+}: {
+  children: React.ReactNode
+} ) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
